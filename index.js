97933492aa022a41d5f56b795383dea05c267572
Noalle Banuelos <noalle_banuelos@yahoo.com>
2023-09-27 23:31:31 -0700

    basic setup

--- index.js
+++ index.js
@@ -5,7 +5,12 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+  // Parse the input string to a number and convert Fahrenheit to Celsius
+  const fahrenheit = parseFloat(fahren);
+  const celsius = (fahrenheit - 32) * (5/9);
+  return celsius;
+}
 
 /**
  * Takes both numbers (F, C) and display a message with
@@ -22,14 +27,39 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahren, celc) {
+  let feeling;
+  if (fahren < 32 || celc < 0) {
+    feeling = "very cold";
+  } else if (fahren < 64 || celc < 18) {
+    feeling = "cold";
+  } else if (fahren < 86 || celc < 30) {
+    feeling = "warm";
+  } else if (fahren < 100 || celc < 37) {
+    feeling = "hot";
+  } else {
+    feeling = "very hot";
+  }
+
+  console.log(`Fahrenheit: ${fahren}°F, Celsius: ${celc}°C - Feels ${feeling}`);
+}
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(limit) {
+  return Math.floor(Math.random() * (limit + 1));
+}
+
+
+const fahrenheit = 75;
+const celsius = convertToCelsius(fahrenheit);
+createMessage(fahrenheit, celsius);
+const randomNum = rand(10);
+console.log(`Random Number: ${randomNum}`);
+
 
 // -------------------- DONT NOT CHANGE THE CODE BELOW ---------------------- //
 
